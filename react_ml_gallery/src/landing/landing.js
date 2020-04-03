afa325995ac1ad2aa1a0bff0b511fa96f2642bcd
Akhilez <akhild18@yahoo.com>
2020-04-03 12:41:02 -0500

    Added landing page with nothing in it

---
+++ react_ml_gallery/src/landing/landing.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class LandingPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <Project />
+            </div>
+        );
+    }
+}
+
+class Project extends React.Component {
+    render() {
+        return (
+            <div>
+                Project
+            </div>
+        );
+    }
+}
+
+export default LandingPage;
