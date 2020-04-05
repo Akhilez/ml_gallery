e4ce8ea33bb6ea980eae8560f7b6a7e319385298
Akhilez <akhild18@yahoo.com>
2020-04-04 23:31:58 -0500

    Added nav color

--- react_ml_gallery/src/commons/ml_app_bar.js
+++ react_ml_gallery/src/commons/ml_app_bar.js
@@ -4,21 +4,18 @@ import Nav from "react-bootstrap/Nav";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import ak_logo from '../commons/images/AK_logo.svg';
+import '../landing/landing.css';
 
 
 class MLAppBar extends React.Component {
     render() {
         return (
             <Navbar bg="transparent" variant="light">
-                <Navbar.Brand href="http://akhilez.com/"><img src={ak_logo} alt={"AK Logo"} height={"40px"} /></Navbar.Brand>
+
                 <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    <Navbar.Brand href="http://akhilez.com/"><img src={ak_logo} alt={"AK Logo"} height={"40px"} /></Navbar.Brand>
                 </Nav>
-                <Form inline>
-                    <Button variant="outline-primary">Search</Button>
-                </Form>
+                <Nav.Link href="/" className={"nav-link"}><div>HOME</div></Nav.Link>
             </Navbar>
         );
     }
