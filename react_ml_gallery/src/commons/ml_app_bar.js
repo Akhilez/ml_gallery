ef458a75265c6f5c48668a7cf0c6e2ac4b98d39f
Akhilez <akhild18@yahoo.com>
2020-04-04 22:05:49 -0500

    Added AK logo

--- react_ml_gallery/src/commons/ml_app_bar.js
+++ react_ml_gallery/src/commons/ml_app_bar.js
@@ -3,13 +3,14 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import ak_logo from '../commons/images/AK_logo.svg';
 
 
 class MLAppBar extends React.Component {
     render() {
         return (
             <Navbar bg="transparent" variant="light">
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Navbar.Brand href="http://akhilez.com/"><img src={ak_logo} alt={"AK Logo"} height={"40px"} /></Navbar.Brand>
                 <Nav className="mr-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
                     <Nav.Link href="#features">Features</Nav.Link>
