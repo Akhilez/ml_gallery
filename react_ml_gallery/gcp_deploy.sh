7feb0cf54cab6aa893d990dc98281d41dcc7a27e
Akhilez <akhild18@yahoo.com>
2020-04-04 19:09:40 -0500

    Added gcp deployment script

---
+++ react_ml_gallery/gcp_deploy.sh
@@ -0,0 +1,4 @@
+gcloud builds submit --tag gcr.io/everst-website/ml-gallery:v1 .
+gcloud beta run deploy ml-gallery --image=gcr.io/everst-website/ml-gallery:v1 --platform managed --allow-unauthenticated --project everst-website
+
+#gcloud config set run/region us-east4
