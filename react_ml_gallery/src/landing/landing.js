a97b30aeae5ca73207cdb938854f824a9b852188
Akhilez <akhild18@yahoo.com>
2020-04-04 23:58:53 -0500

    Added project image

--- react_ml_gallery/src/landing/landing.js
+++ react_ml_gallery/src/landing/landing.js
@@ -3,6 +3,9 @@ import Container from "@material-ui/core/Container";
 import MLAppBar from '../commons/ml_app_bar';
 import MLLogo from "./ml_logo/ml_logo";
 import 'cytoscape/dist/cytoscape.min';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import colorizerImage from './images/colorizer.jpg';
 
 
 class LandingPage extends React.Component {
@@ -12,6 +15,7 @@ class LandingPage extends React.Component {
                 <MLAppBar/>
                 <MLLogo/>
                 <Project/>
+                <Footer/>
             </Container>
         );
     }
@@ -21,10 +25,25 @@ class Project extends React.Component {
     render() {
         return (
             <div>
-                Project
+                <Row>
+                    <Col>
+                        <div className={"projectImageContainer"}>
+                            <img src={colorizerImage} className={"project-image"} alt={"ColorizerImage"}/>
+                        </div>
+                    </Col>
+                    <Col>
+                        Text
+                    </Col>
+                </Row>
             </div>
         );
     }
+}
+
+function Footer () {
+    return (
+        <div><br/><hr/><br/>Footer<br/></div>
+    );
 }
 
 export default LandingPage;
