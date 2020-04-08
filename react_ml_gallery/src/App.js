2641d585db2a89c90a0a1fecdbdb33eb3864529a
Akhilez <akhild18@yahoo.com>
2020-04-07 22:11:26 -0500

    Started MnistGan

--- react_ml_gallery/src/App.js
+++ react_ml_gallery/src/App.js
@@ -2,11 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LandingPage from "./landing/landing";
+import MnistGanPage from "./mnist_gan";
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+export default function App() {
   return (
-    <LandingPage />
+      <Router>
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/mnist_gan">
+              <MnistGanPage />
+            </Route>
+            <Route path="/">
+              <LandingPage />
+            </Route>
+          </Switch>
+      </Router>
   );
 }
 
-export default App;
---
+++ react_ml_gallery/src/mnist_gan/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class MnistGanPage extends React.Component {
+    render() {
+        return (
+            <p>Hiiiiiiiiii</p>
+        )
+    }
+}
