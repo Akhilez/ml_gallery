24822f82367abce2e7c4815b02cd3aac0cc7be07
Akhilez <akhild18@yahoo.com>
2020-04-15 16:14:54 -0500

    Added some more gan stuff

--- ml_py/MLGallery/selective_generator/model.py
+++ ml_py/MLGallery/selective_generator/model.py
@@ -1,10 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy import load
-
+import torch
+import torchvision
 
 data_path = "../../data/mnist/"
 
-data = load(f'{data_path}/x_test.npy')
+train_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.MNIST(data_path, train=True, download=True,
+                               transform=torchvision.transforms.ToTensor()), shuffle=True)
 
-print(data.shape)
+test_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.MNIST(data_path, train=False, download=True,
+                               transform=torchvision.transforms.ToTensor()), shuffle=True)
+
+
+x_train = []
+x_test = []
+y_test = []
+y_train = []
+
+for data in test_loader:
+    data[1]
+    x_test.append()
+test = list(test_loader)
+print()
