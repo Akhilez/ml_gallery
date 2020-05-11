71fdf6fe72db2ecceb5ef4d3be3a5e3eed126e86
Akhilez <lightrescuer@gmail.com>
2020-05-11 13:12:46 -0500

    Added a technique to communicate b/w clint to server

--- ml_py/experiments/chat/consumers.py
+++ ml_py/experiments/chat/consumers.py
@@ -4,6 +4,12 @@ from channels.generic.websocket import WebsocketConsumer
 
 
 class ChatConsumer(WebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.trace_id = None  # Manage the trace using a DB or some redis queue or whatever that is stateful
+        # Maintain a list of trace ids
+        self.last_message = None
+
     def connect(self):
         self.accept()
 
@@ -12,11 +18,21 @@ class ChatConsumer(WebsocketConsumer):
 
     def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
 
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+        if text_data_json['type'] == 'sendMessage':
+            if self.trace_id is None:
+                self.trace_id = text_data_json['traceId']
+
+            message = text_data_json['message']
+            self.last_message = message
+
+            self.send(text_data=json.dumps({
+                'message': message
+            }))
+        elif text_data_json['type'] == 'action':
+            if self.trace_id is not None:
+                if self.last_message is not None:
+                    self.send(text_data=json.dumps({'message': self.last_message[::-1]}))
 
         time.sleep(5)
 
