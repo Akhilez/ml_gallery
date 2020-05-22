bcd604bdf40240af1cf0e3b62c63487cace600e1
Akhilez <lightrescuer@gmail.com>
2020-05-22 15:15:52 -0500

    Made py.ml.gallery good for deployment

---
+++ ml_py/local_deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -x
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+
+docker build -t py-ml-gallery:v1 $DIR
+
+docker run --publish 8000:8000 py-ml-gallery:v1
+
+# docker ps -a
+# docker rm <container id> [<container id>,]
+
+# docker images
+# docker rmi <image id> [<image id>,]
--- ml_py/ml_py/routing.py
+++ ml_py/ml_py/routing.py
@@ -1,15 +1,17 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from experiments.chat import routing as chat_routing
+from channels.security.websocket import AllowedHostsOriginValidator
 from MLGallery.regressors.polynomial import routing as poly_reg_routing
-
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            chat_routing.websocket_urlpatterns +
-            poly_reg_routing.websocket_urlpatterns
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                chat_routing.websocket_urlpatterns +
+                poly_reg_routing.websocket_urlpatterns
+            )
         )
     ),
 })
