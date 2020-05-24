ff73283d7f5e6d9e62dac232ab970966653d82e2
Akhilez <lightrescuer@gmail.com>
2020-05-24 11:26:29 -0500

    Changed gcloud project

--- react_ml_gallery/gcp_deploy.sh
+++ react_ml_gallery/gcp_deploy.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-gcloud builds submit --tag gcr.io/everst-website/ml-gallery:v1 .
-gcloud beta run deploy ml-gallery --image=gcr.io/everst-website/ml-gallery:v1 --platform managed --allow-unauthenticated --project everst-website
+PROJECT_ID="graphic-jet-278213"
+IMAGE_NAME="ml-gallery"
+
+gcloud builds submit --tag gcr.io/$PROJECT_ID/$IMAGE_NAME:v1 $DIR
+
+gcloud beta run deploy $IMAGE_NAME --image=gcr.io/$PROJECT_ID/$IMAGE_NAME:v1 --allow-unauthenticated --memory=512Mi --timeout=900 --platform managed --set-env-vars=DEBUG=False
 
 #gcloud config set run/region us-east4
