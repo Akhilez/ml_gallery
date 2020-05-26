0be55af3c6d9c7d9f2070e2054779069c899b37a
Akhilez <lightrescuer@gmail.com>
2020-05-25 20:43:56 -0500

    Added aug code

--- ml_py/lib/mnist_aug/mnist_augmenter.py
+++ ml_py/lib/mnist_aug/mnist_augmenter.py
@@ -1,9 +1,53 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import torch
+from random import random
+from skimage.transform import resize
 
 
 class MNISTAug:
     def __init__(self):
         self.dm = DataManager()
+        self.scale = 4
+        self.overflow = 0.5
+        self.min_numbers_out = 4
+        self.max_numbers_out = 10
+
+    def get_augmented(self, x: np.ndarray, y: np.ndarray, n_out: int):
+        """
+
+        Parameters
+        ----------
+        x: a tensor of shape [1000, 28, 28]
+        y: a tensor of shape [1000, 1]
+        n_out: number of output images
+
+        Returns
+        -------
+        aug_x: a tensor of shape [1000, 112, 112]
+        aug_y: a tensor of shape [n_out, numbers_out, 5] | 5 => [class, x1, y1, x2, y2]
+
+        """
+
+        self.dm.load_test()
+
+        x_in = x.shape[1]
+        x_out = x.shape[1] * self.scale
+
+        aug_x = np.zeros((x.shape[0], x_out, x_out))
+
+        i = 0
+
+        rand_x = int(random() * x_in * (self.scale - self.overflow))
+        rand_y = int(random() * x_in * (self.scale - self.overflow))
+
+        localized_dim_x = min(aug_x - rand_x, x_in)
+        localized_dim_y = min(aug_x - rand_y, x_in)
+
+        localized_xi = x[i][:localized_dim_x, :localized_dim_y]
+        aug_x[i][rand_x:rand_x + localized_dim_x, rand_y:rand_y + localized_dim_y] += localized_xi
+
+        return aug_x, aug_y
 
 
 class DataManager:
@@ -24,3 +68,7 @@ class DataManager:
     def load_test(self):
         self.x_test = np.load(f'{self.dir}/x_test.npy')
         self.y_test = np.load(f'{self.dir}/y_test.npy')
+
+    @staticmethod
+    def plot_num(x):
+        plt.imshow(x, cmap='gray')
