d2ce15c4213240fcc6f3f5d18efbd86ea6306aef
Akhilez <lightrescuer@gmail.com>
2020-06-03 10:49:58 -0500

    Fixed

--- ml_js/deploy/gcp.sh
+++ ml_js/deploy/gcp.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 # set -x
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR/..
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
+cd $DIR
 echo "PWD: `pwd`"
 
 PROJECT_ID="graphic-jet-278213"
 IMAGE_NAME="ml-gallery"
 
-echo gcloud builds submit --tag gcr.io/$PROJECT_ID/$IMAGE_NAME:v1 $DIR
+gcloud builds submit --tag gcr.io/$PROJECT_ID/$IMAGE_NAME:v1 `pwd`
 
-echo gcloud beta run deploy $IMAGE_NAME --image=gcr.io/$PROJECT_ID/$IMAGE_NAME:v1 --allow-unauthenticated --memory=512Mi --timeout=900 --platform managed --set-env-vars=DEBUG=False
+gcloud beta run deploy $IMAGE_NAME --image=gcr.io/$PROJECT_ID/$IMAGE_NAME:v1 --allow-unauthenticated --memory=512Mi --timeout=900 --platform managed --set-env-vars=DEBUG=False
 
 # gcloud builds submit --tag gcr.io/graphic-jet-278213/ml-gallery:v1 .
 # gcloud beta run deploy ml-gallery --image=gcr.io/graphic-jet-278213/ml-gallery:v1 --allow-unauthenticated --memory=512Mi --timeout=900 --platform managed --set-env-vars=DEBUG=False
