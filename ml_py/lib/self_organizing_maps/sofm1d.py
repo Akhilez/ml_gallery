232e548a2eb96e9ae5bb270a8bf5977ee36abfda
Akhilez <lightrescuer@gmail.com>
2020-06-12 22:12:51 -0500

    Working on SOM

--- ml_py/lib/self_organizing_maps/sofm1d.py
+++ ml_py/lib/self_organizing_maps/sofm1d.py
@@ -7,9 +7,11 @@ class SOFM1D(torch.nn.Module):
     COSINE_OP = 'cosine'
     DISTANCE_OP = 'distance'
 
-    def __init__(self, input_size, output_size, operation=DISTANCE_OP):
+    def __init__(self, input_size, output_size, lr=0.1, operation=DISTANCE_OP):
         super().__init__()
-        self.weights = get_scaled_random_weights((input_size, output_size), -1, 1)
+        self.w = get_scaled_random_weights((input_size, output_size), -1, 1)
+        self.w.requires_grad = False
+        self.lr = lr
 
     def forward(self, x):
         """
@@ -17,17 +19,16 @@ class SOFM1D(torch.nn.Module):
         2. i* = arg min of distances
         3. Get neuron_distance factor
         4.
-
         Parameters
         ----------
         x: batch of 1D Tensors
-
-        Returns
-        -------
-
         """
-
-        pass  # TODO: Do the operation
+        deltas = []
+        y_hat = []
+        for xi in x:
+            differences = torch.sum((self.w.T - xi) ** 2, dim=1)
+            i_min = torch.argmin(differences)
+            deltas.append()
 
     def backward(self):
         pass
