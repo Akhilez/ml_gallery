13049707f84076cec471070859353d945540a294
Akhilez <lightrescuer@gmail.com>
2020-06-29 20:18:25 -0500

    Added a dummy commit

--- ml_py/MLGallery/vision/mnist_classifier/mnist_classifier.py
+++ ml_py/MLGallery/vision/mnist_classifier/mnist_classifier.py
@@ -3,5 +3,8 @@ import tensorflow as tf
 
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Conv2D(),
+    tf.keras.layers.Conv2D(1, 16), # TODO: This is pytorch, make it keras like
+    tf.keras.layers.Conv2D(16, 32),
 ])
+
+model.compile()
