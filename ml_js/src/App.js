fbffed35e3df721fbd0fbfc13c87940cb018abfc
Akhilez <lightrescuer@gmail.com>
2020-08-19 17:22:27 -0500

    Reverted profile

--- ml_js/src/App.js
+++ ml_js/src/App.js
@@ -9,6 +9,7 @@ import LearnLinePage from "./feed_forward/learn_line/learn_line";
 import LinearClassifierPage from "./feed_forward/linear_classifier/linear_classifier";
 import LearnCurvePage from "./feed_forward/curve/learn_curve";
 import ProfilePage from "./profile/profile";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { TitleComponent } from "./commons/components/components";
 import ResumePage from "./profile/resume";
 import urls from "./urls";
@@ -16,50 +17,45 @@ import AllProjectsPage from "./profile/all_projects";
 import DeepIrisPage from "./feed_forward/deep_iris/deep_iris";
 import WhichCharPage from "./vision/which_char/which_char";
 import NumberDetectorPage from "./vision/num_detector/num_detector";
-import { ThemeProvider } from "@chakra-ui/core";
-import { Router } from "@reach/router";
 
 export default class App extends React.Component {
   render() {
     // tf.setBackend('wasm');
     return (
-      <>
-        <Router></Router>
-        <Router>
-          <Switch>
-            {projects.categories.map((category) =>
-              category.projects.map((project) => (
-                <Route path={project.links.app} key={project.id}>
-                  {this.getProjectComponent(project)}
-                </Route>
-              ))
-            )}
-            <Route
-              path={urls.resume.url}
-              component={this.withTitle({
-                component: ResumePage,
-                title: urls.resume.title,
-              })}
-            />
-            <Route path={urls.ml_gallery.url} component={LandingPage} />
-            <Route
-              path={urls.all_projects.url}
-              component={this.withTitle({
-                component: AllProjectsPage,
-                title: urls.all_projects.title,
-              })}
-            />
+      <Router>
+        <Switch>
+          {projects.categories.map((category) =>
+            category.projects.map((project) => (
+              <Route path={project.links.app} key={project.id}>
+                {this.getProjectComponent(project)}
+              </Route>
+            ))
+          )}
+          <Route
+            path={urls.resume.url}
+            component={this.withTitle({
+              component: ResumePage,
+              title: urls.resume.title,
+            })}
+          />
+          <Route path={urls.ml_gallery.url} component={LandingPage} />
+          <Route
+            path={urls.all_projects.url}
+            component={this.withTitle({
+              component: AllProjectsPage,
+              title: urls.all_projects.title,
+            })}
+          />
 
-            <Route
-              path={urls.profile.url}
-              component={this.withTitle({
-                component: ProfilePage,
-                title: urls.profile.title,
-              })}
-            />
-          </Switch>
-        </Router>
-      </>
+          <Route
+            path={urls.profile.url}
+            component={this.withTitle({
+              component: ProfilePage,
+              title: urls.profile.title,
+            })}
+          />
+        </Switch>
+      </Router>
     );
   }
 
