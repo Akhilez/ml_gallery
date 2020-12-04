3be308acc5c784c537baad95f41e361a3c1bd24b
Akhilez <lightrescuer@gmail.com>
2020-12-03 22:54:35 -0600

    Finished some IOU testing

--- ml_py/app/vision/dense_cap/detection_test.py
+++ ml_py/app/vision/dense_cap/detection_test.py
@@ -24,7 +24,7 @@ def labels_to_tensor(labels):
             ]
             tensor[i] = torch.FloatTensor(coordinates)
 
-        tensors.append(tensor)
+        tensors.append(tensor.T)
 
     return tensors
 
@@ -40,8 +40,10 @@ def get_shapes_from_sizes_ratios(sizes, ratios):
 
 
 def get_anchor_centers(W, H):
-    hs = torch.arange(1/H/2, 1, 1/H).view((H, 1)).expand((H, W))  # [0, 0.5, 1] => [(0,0,0), (0.5,0.5,0.5), (1,1,1)]
-    ws = torch.arange(1/W/2, 1, 1/W).view((1, W)).expand((H, W))  # [0, 0.5, 1] => [(0,0.5,1), (0,0.5,1), (0,0.5,1)]
+    hs = torch.arange(1 / H / 2, 1, 1 / H).view((H, 1)).expand(
+        (H, W))  # [0, 0.5, 1] => [(0,0,0), (0.5,0.5,0.5), (1,1,1)]
+    ws = torch.arange(1 / W / 2, 1, 1 / W).view((1, W)).expand(
+        (H, W))  # [0, 0.5, 1] => [(0,0.5,1), (0,0.5,1), (0,0.5,1)]
 
     return ws, hs  # 2 channels
 
@@ -60,200 +62,221 @@ def generate_anchors(shape, sizes, ratios):
     w = w.view((1, -1)).expand((H * W, -1)).flatten()
     h = h.view((1, -1)).expand((H * W, -1)).flatten()
 
-    return torch.stack((cx, cy, w, h)).T
+    return torch.stack((cx, cy, w, h))
 
 
 def get_iou_map(boxes1, boxes2):
+    n1 = boxes1.shape[1]
+    n2 = boxes2.shape[1]
 
-    n1 = boxes1.shape[0]
-    n2 = boxes2.shape[0]
+    boxes1 = boxes1.repeat_interleave(n2).reshape((4, n1 * n2))  # [1, 2] => [1, 1, 2, 2]
+    boxes2 = boxes2.repeat((1, n1))  # [1, 2] => [1, 2, 1, 2]
 
-    boxes1 = boxes1.T.repeat_interleave(n2).reshape((4, n1 * n2))  # [1, 2] => [1, 1, 2, 2]
-    boxes2 = boxes2.T.repeat((1, n1))  # [1, 2] => [1, 2, 1, 2]
+    wb = boxes1[2]
+    wa = boxes2[2]
 
-    w1 = boxes1[2]
-    w2 = boxes2[2]
+    hb = boxes1[3]
+    ha = boxes2[3]
 
-    h1 = boxes1[3]
-    h2 = boxes2[3]
+    cxb = boxes1[0]
+    cxa = boxes2[0]
 
-    cx1 = boxes1[0]
-    cx2 = boxes2[0]
+    cyb = boxes1[1]
+    cya = boxes2[1]
 
-    cy1 = boxes1[1]
-    cy2 = boxes2[1]
+    wb_half = wb / 2
+    hb_half = hb / 2
 
-    w = ((w1 + w2) / 2) - torch.abs(cx1 - cx2)
-    h = ((h1 + h2) / 2) - torch.abs(cy1 - cy2)
+    x2b = cxb + wb_half
+    x1b = cxb - wb_half
+
+    y2b = cyb + hb_half
+    y1b = cyb - hb_half
+
+    wa_half = wa / 2
+    ha_half = ha / 2
+
+    x2a = cxa + wa_half
+    x1a = cxa - wa_half
+
+    y2a = cya + ha_half
+    y1a = cya - ha_half
+
+    x2 = torch.min(torch.stack((x2a, x2b)), 0)[0]
+    x1 = torch.max(torch.stack((x1a, x1b)), 0)[0]
+
+    y2 = torch.min(torch.stack((y2a, y2b)), 0)[0]
+    y1 = torch.max(torch.stack((y1a, y1b)), 0)[0]
+
+    w = x2 - x1
+    h = y2 - y1
+
+    w[w < 0] = 0
+    h[h < 0] = 0
 
     intersection = w * h
-    intersection[intersection < 0] = 0
-
-    a1 = w1 * h1
-    a2 = w2 * h2
-
-    union = a1 + a2 - intersection
-
+    union = (wb * hb) + (wa * ha) - intersection
     iou = intersection / union
 
     return iou.view((n1, n2))
 
 
-y = [
-    [
-        {'id': 0,
-         'class': 9,
-         'class_one_hot': [0., 0., 0., 0., 0., 0., 0., 0., 0., 1.],
-         'x1': 40,
-         'y1': 26,
-         'x2': 70,
-         'y2': 56,
-         'cx': 55.0,
-         'cy': 41.0,
-         'height': 30,
-         'width': 30,
-         'type': 'number'},
-        {'id': 1,
-         'class': 3,
-         'class_one_hot': [0., 0., 0., 1., 0., 0., 0., 0., 0., 0.],
-         'x1': 13,
-         'y1': 78,
-         'x2': 50,
-         'y2': 112,
-         'cx': 31.5,
-         'cy': 95.0,
-         'height': 34,
-         'width': 37,
-         'type': 'number'},
-        {'id': 2,
-         'class': 6,
-         'class_one_hot': [0., 0., 0., 0., 0., 0., 1., 0., 0., 0.],
-         'x1': 78,
-         'y1': 35,
-         'x2': 112,
-         'y2': 70,
-         'cx': 95.0,
-         'cy': 52.5,
-         'height': 35,
-         'width': 34,
-         'type': 'number'},
-        {'id': 3,
-         'class': 1,
-         'class_one_hot': [0., 1., 0., 0., 0., 0., 0., 0., 0., 0.],
-         'x1': 35,
-         'y1': 46,
-         'x2': 84,
-         'y2': 95,
-         'cx': 59.5,
-         'cy': 70.5,
-         'height': 49,
-         'width': 49,
-         'type': 'number'},
-        {'id': 4,
-         'class': 2,
-         'class_one_hot': [0., 0., 1., 0., 0., 0., 0., 0., 0., 0.],
-         'x1': 20,
-         'y1': 55,
-         'x2': 42,
-         'y2': 77,
-         'cx': 31.0,
-         'cy': 66.0,
-         'height': 22,
-         'width': 22,
-         'type': 'number'},
-        {'id': 5,
-         'class': 1,
-         'class_one_hot': [0., 1., 0., 0., 0., 0., 0., 0., 0., 0.],
-         'x1': 60,
-         'y1': 11,
-         'x2': 95,
-         'y2': 46,
-         'cx': 77.5,
-         'cy': 28.5,
-         'height': 35,
-         'width': 35,
-         'type': 'number'},
-        {'id': 6,
-         'class': 1,
-         'class_one_hot': [0., 1., 0., 0., 0., 0., 0., 0., 0., 0.],
-         'x1': 60,
-         'y1': 79,
-         'x2': 102,
-         'y2': 112,
-         'cx': 81.0,
-         'cy': 95.5,
-         'height': 33,
-         'width': 42,
-         'type': 'number'},
-        {'id': 7,
-         'class': 8,
-         'class_one_hot': [0., 0., 0., 0., 0., 0., 0., 0., 1., 0.],
-         'x1': 4,
-         'y1': 14,
-         'x2': 51,
-         'y2': 61,
-         'cx': 27.5,
-         'cy': 37.5,
-         'height': 47,
-         'width': 47,
-         'type': 'number'}],
-    [
-        {'id': 0,
-         'class': 4,
-         'class_one_hot': [0., 0., 0., 0., 1., 0., 0., 0., 0., 0.],
-         'x1': 33,
-         'y1': 28,
-         'x2': 87,
-         'y2': 82,
-         'cx': 60.0,
-         'cy': 55.0,
-         'height': 54,
-         'width': 54,
-         'type': 'number'},
-        {'id': 1,
-         'class': 0,
-         'class_one_hot': [1., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
-         'x1': 68,
-         'y1': 69,
-         'x2': 103,
-         'y2': 104,
-         'cx': 85.5,
-         'cy': 86.5,
-         'height': 35,
-         'width': 35,
-         'type': 'number'},
-        {'id': 2,
-         'class': 1,
-         'class_one_hot': [0., 1., 0., 0., 0., 0., 0., 0., 0., 0.],
-         'x1': 62,
-         'y1': 0,
-         'x2': 95,
-         'y2': 33,
-         'cx': 78.5,
-         'cy': 16.5,
-         'height': 33,
-         'width': 33,
-         'type': 'number'},
-        {'id': 3,
-         'class': 1,
-         'class_one_hot': [0., 1., 0., 0., 0., 0., 0., 0., 0., 0.],
-         'x1': 16,
-         'y1': 88,
-         'x2': 48,
-         'y2': 112,
-         'cx': 32.0,
-         'cy': 100.0,
-         'height': 24,
-         'width': 32,
-         'type': 'number'}]]
+if __name__ == '__main__':
+    y = [
+        [
+            {'id': 0,
+             'class': 9,
+             'class_one_hot': [0., 0., 0., 0., 0., 0., 0., 0., 0., 1.],
+             'x1': 40,
+             'y1': 26,
+             'x2': 70,
+             'y2': 56,
+             'cx': 55.0,
+             'cy': 41.0,
+             'height': 30,
+             'width': 30,
+             'type': 'number'},
+            {'id': 1,
+             'class': 3,
+             'class_one_hot': [0., 0., 0., 1., 0., 0., 0., 0., 0., 0.],
+             'x1': 13,
+             'y1': 78,
+             'x2': 50,
+             'y2': 112,
+             'cx': 31.5,
+             'cy': 95.0,
+             'height': 34,
+             'width': 37,
+             'type': 'number'},
+            {'id': 2,
+             'class': 6,
+             'class_one_hot': [0., 0., 0., 0., 0., 0., 1., 0., 0., 0.],
+             'x1': 78,
+             'y1': 35,
+             'x2': 112,
+             'y2': 70,
+             'cx': 95.0,
+             'cy': 52.5,
+             'height': 35,
+             'width': 34,
+             'type': 'number'},
+            {'id': 3,
+             'class': 1,
+             'class_one_hot': [0., 1., 0., 0., 0., 0., 0., 0., 0., 0.],
+             'x1': 35,
+             'y1': 46,
+             'x2': 84,
+             'y2': 95,
+             'cx': 59.5,
+             'cy': 70.5,
+             'height': 49,
+             'width': 49,
+             'type': 'number'},
+            {'id': 4,
+             'class': 2,
+             'class_one_hot': [0., 0., 1., 0., 0., 0., 0., 0., 0., 0.],
+             'x1': 20,
+             'y1': 55,
+             'x2': 42,
+             'y2': 77,
+             'cx': 31.0,
+             'cy': 66.0,
+             'height': 22,
+             'width': 22,
+             'type': 'number'},
+            {'id': 5,
+             'class': 1,
+             'class_one_hot': [0., 1., 0., 0., 0., 0., 0., 0., 0., 0.],
+             'x1': 60,
+             'y1': 11,
+             'x2': 95,
+             'y2': 46,
+             'cx': 77.5,
+             'cy': 28.5,
+             'height': 35,
+             'width': 35,
+             'type': 'number'},
+            {'id': 6,
+             'class': 1,
+             'class_one_hot': [0., 1., 0., 0., 0., 0., 0., 0., 0., 0.],
+             'x1': 60,
+             'y1': 79,
+             'x2': 102,
+             'y2': 112,
+             'cx': 81.0,
+             'cy': 95.5,
+             'height': 33,
+             'width': 42,
+             'type': 'number'},
+            {'id': 7,
+             'class': 8,
+             'class_one_hot': [0., 0., 0., 0., 0., 0., 0., 0., 1., 0.],
+             'x1': 4,
+             'y1': 14,
+             'x2': 51,
+             'y2': 61,
+             'cx': 27.5,
+             'cy': 37.5,
+             'height': 47,
+             'width': 47,
+             'type': 'number'}],
+        [
+            {'id': 0,
+             'class': 4,
+             'class_one_hot': [0., 0., 0., 0., 1., 0., 0., 0., 0., 0.],
+             'x1': 33,
+             'y1': 28,
+             'x2': 87,
+             'y2': 82,
+             'cx': 60.0,
+             'cy': 55.0,
+             'height': 54,
+             'width': 54,
+             'type': 'number'},
+            {'id': 1,
+             'class': 0,
+             'class_one_hot': [1., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+             'x1': 68,
+             'y1': 69,
+             'x2': 103,
+             'y2': 104,
+             'cx': 85.5,
+             'cy': 86.5,
+             'height': 35,
+             'width': 35,
+             'type': 'number'},
+            {'id': 2,
+             'class': 1,
+             'class_one_hot': [0., 1., 0., 0., 0., 0., 0., 0., 0., 0.],
+             'x1': 62,
+             'y1': 0,
+             'x2': 95,
+             'y2': 33,
+             'cx': 78.5,
+             'cy': 16.5,
+             'height': 33,
+             'width': 33,
+             'type': 'number'},
+            {'id': 3,
+             'class': 1,
+             'class_one_hot': [0., 1., 0., 0., 0., 0., 0., 0., 0., 0.],
+             'x1': 16,
+             'y1': 88,
+             'x2': 48,
+             'y2': 112,
+             'cx': 32.0,
+             'cy': 100.0,
+             'height': 24,
+             'width': 32,
+             'type': 'number'}]]
 
-y_ = labels_to_tensor(y)
+    y_ = labels_to_tensor(y)
 
-anchors_tensor = generate_anchors(shape=(W, H), sizes=(.15, .45, .75), ratios=(0.5, 1, 2))
+    anchors_tensor = generate_anchors(shape=(W, H), sizes=(.15, .45, .75), ratios=(0.5, 1, 2))
 
-iou = get_iou_map(y_[1], anchors_tensor)
+    iou = get_iou_map(y_[1], anchors_tensor)
 
-print(anchors_tensor.shape)
-print([y_i.shape for y_i in y_])
-print(iou.flatten().tolist())
-
+    print(anchors_tensor.shape)
+    print([y_i.shape for y_i in y_])
+    print(iou.shape)
+    # print(iou.flatten().tolist())
---
+++ ml_py/app/vision/dense_cap/test_detection_utils.py
@@ -0,0 +1,25 @@
+import unittest
+import torch
+
+from app.vision.dense_cap.detection_test import get_iou_map
+
+
+class TestDetectionUtils(unittest.TestCase):
+    def test_get_iou_map_1(self):
+        b1 = torch.FloatTensor([
+            [3, 3, 4, 4],
+            [7, 7, 4, 4]
+        ]).T / 10
+
+        b2 = torch.FloatTensor([
+            [3, 5, 2, 2],
+            [7, 7, 2, 2]
+        ]).T / 10
+
+        iou = get_iou_map(b1, b2)
+
+        iou_label = torch.FloatTensor([[1 / 9, 0], [0, 0.25]])
+
+        diff = float(torch.abs(iou - iou_label).max())
+
+        self.assertGreater(0.00001, diff)
