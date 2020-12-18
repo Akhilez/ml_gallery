3235dbd507aa6260e16446e41e491e0e89575b93
Akhilez <lightrescuer@gmail.com>
2020-12-17 23:45:49 -0600

    Started trainer

--- ml_py/app/vision/find_all_chars/find_all_trainer.py
+++ ml_py/app/vision/find_all_chars/find_all_trainer.py
@@ -5,23 +5,18 @@ from app.vision.find_all_chars.model import MnistDetector
 
 
 def train(model, train_set, epochs, batch_size, test_set=None):
-
-    train_loader = DataLoader(train_set, shuffle=True, batch_size=batch_size)
+    train_loader = DataLoader(train_set, shuffle=True, batch_size=batch_size, collate_fn=lambda x: x)
 
     for epoch in range(epochs):
         for i_batch, batch in enumerate(train_loader):
             print(i_batch)
 
-            x = batch['x']
-            y = batch['y']
-
-            print(x.shape)
-            print(y)
+            print(batch[0]['x'].shape)
+            print(batch[0]['y'][0])
             return
 
 
 def main():
-
     train_set = MNISTAugDataset(10)
     test_set = MNISTAugDataset(2)
 
