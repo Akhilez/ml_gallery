80cfec3a8f238582f8dea934733033796e948a0a
Akhilez <lightrescuer@gmail.com>
2020-12-19 08:28:44 -0600

    Working on training detector

--- ml_py/app/vision/find_all_chars/find_all_trainer.py
+++ ml_py/app/vision/find_all_chars/find_all_trainer.py
@@ -1,7 +1,11 @@
 from torch.utils.data import DataLoader
+import torch
 
 from lib.mnist_aug.loader import MNISTAugDataset
 from app.vision.find_all_chars.model import MnistDetector
+from lib import detection_utils as utils
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 def train(model, train_set, epochs, batch_size, test_set=None):
@@ -9,16 +13,19 @@ def train(model, train_set, epochs, batch_size, test_set=None):
 
     for epoch in range(epochs):
         for i_batch, batch in enumerate(train_loader):
-            print(i_batch)
 
-            print(batch[0]['x'].shape)
-            print(batch[0]['y'][0])
+            x_batch = torch.tensor([xi['x'] for xi in batch], device=device)
+            y_batch = [yi['y'] for yi in batch]
+
+            y_boxes = [utils.labels_to_tensor(yi, H, W) for yi in y_batch]
+            detector_out = model(x_batch, y_boxes)
+
             return
 
 
 def main():
     train_set = MNISTAugDataset(10)
-    test_set = MNISTAugDataset(2)
+    test_set = MNISTAugDataset(2, test_mode=True)
 
     model = MnistDetector()
 
