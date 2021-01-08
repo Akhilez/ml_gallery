dac5472cc3c9e98244e29d75f9c8be6dd9a56136
Akhilez <lightrescuer@gmail.com>
2021-01-08 15:39:57 -0600

    tested installation of all packages

--- ml_py/deploy/gcp_deploy.sh
+++ ml_py/deploy/gcp_deploy.sh
@@ -8,11 +8,7 @@ source ./.env
 
 gcloud builds submit --tag gcr.io/$PROJECT_ID/$IMAGE_NAME:v1 $DIR
 
-gcloud beta run deploy $CLOUD_RUN_SERVICE --image=gcr.io/$PROJECT_ID/$IMAGE_NAME:v1 --allow-unauthenticated --memory=2048Mi --timeout=900 --platform managed --set-env-vars=DEBUG=False
-
-# WINDOWS:
-# gcloud builds submit --tag gcr.io/lateral-imagery-267008/plabmaster_web:v1 .
-# gcloud beta run deploy plabmaster-web --image=gcr.io/lateral-imagery-267008/plabmaster_web:v1 --allow-unauthenticated --memory=512Mi --timeout=900 --platform managed
+gcloud beta run deploy $IMAGE_NAME --image=gcr.io/$PROJECT_ID/$IMAGE_NAME:v1 --allow-unauthenticated --memory=2048Mi --timeout=900 --platform managed --set-env-vars=DEBUG=False
 
 # Set default region:
 # gcloud config set run/region asia-east1
