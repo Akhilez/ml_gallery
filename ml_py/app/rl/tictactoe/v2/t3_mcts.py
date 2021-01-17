ad3d5a7831052a5e35fa8220547d2de973d4a6eb
Akhilez <lightrescuer@gmail.com>
2021-01-17 01:30:10 -0600

    finished mcts

--- ml_py/app/rl/tictactoe/v2/t3_mcts.py
+++ ml_py/app/rl/tictactoe/v2/t3_mcts.py
@@ -1,6 +1,5 @@
 from gym_tic_tac_toe.envs import TicTacToeEnvV2
 import numpy as np
-import torch
 import copy
 
 env = TicTacToeEnvV2()
@@ -15,10 +14,16 @@ class MctsNode:
         self.action = action
         self.n = 0
         self.wins = 0
-        self.is_learner = True if parent is None else not parent.is_learner
+        self.is_learner = False if parent is None else not parent.is_learner
         self.state = copy.deepcopy(parent.state) if parent is not None else np.zeros(9)
         if action is not None:
             self.state[action] = learner if self.is_learner else -learner
+
+    def __str__(self):
+        return f'({self.wins}/{self.n})'
+
+    def __repr__(self):
+        return str(self)
 
 
 """
@@ -44,7 +49,7 @@ def uct(node):
 
 
 def select(node):
-    if node.n != 0 and node.parent is not None:
+    if node.n != 0 and node.children is not None:
         idx = np.argmax([uct(child) for child in node.children])
         return select(node.children[idx])
     return node
@@ -64,8 +69,10 @@ def rollout(state, turn):
     actions = env.get_legal_actions()
     action = np.random.choice(actions)
     state, _, done, _ = env.step(action)
-    if done and env.winner is not None:
-        return env.winner * learner
+    if done:
+        if env.winner is not None:
+            return max(0, env.winner * learner)
+        return 0
     return rollout(state, env.turn)
 
 
@@ -79,16 +86,32 @@ def backprop(node, win):
 def iter_mcts():
     node = select(tree)
     node = expand(node)
-    win = rollout(node.state, learner if node.is_learner else -learner)
+    win = rollout(copy.deepcopy(node.state), learner if node.is_learner else -learner)
     backprop(node, win)
 
 
-def print_tree(root):
-    pass
+def print_tree(nodes, max_depth=4):
+    if max_depth < 1:
+        return
+    new_nodes = []
+    for siblings in nodes:
+        if siblings is None:
+            continue
+        for node in siblings:
+            if node.parent is None:
+                print(node)
+            if node.children is not None:
+                children = [child for child in node.children if child is not None]
+                new_nodes.append(children)
+            else:
+                new_nodes.append(None)
+    if len(new_nodes) != 0:
+        print(new_nodes)
+        return print_tree(new_nodes, max_depth - 1)
 
 
-iterations = 10
+iterations = 800
 for i in range(iterations):
     iter_mcts()
 
-print_tree(tree)
+print_tree([[tree]])
