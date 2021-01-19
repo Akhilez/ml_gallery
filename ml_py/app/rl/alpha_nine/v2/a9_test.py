990532ccb726217516fbdf270fcb28f8cf281047
Akhilez <lightrescuer@gmail.com>
2021-01-18 23:25:03 -0600

    Done with v2 env of 9 mens

---
+++ ml_py/app/rl/alpha_nine/v2/a9_test.py
@@ -0,0 +1,28 @@
+import numpy as np
+from gym_nine_mens_morris.envs.nmm_v2 import NineMensMorrisEnvV2
+
+
+def play(p1, p2, render=False):
+    env = NineMensMorrisEnvV2()
+    env.reset()
+
+    while not env.done:
+        p = p1 if env.turn == 1 else p2
+        action = p(env)
+        state, reward, done, info = env.step(action)
+        print(reward, done, info)
+        if render:
+            env.render()
+
+    return env.winner
+
+
+def random_player(env):
+    actions, opponents = env.get_legal_actions()
+    action = actions[np.random.choice(range(len(actions)))]
+    if action[2]:
+        action[2] = np.random.choice(opponents)
+    return action
+
+
+print(play(random_player, random_player, True))
