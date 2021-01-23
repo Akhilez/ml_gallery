dbe68891ce3d99d5db2861f0e2a76519362f978e
Akhilez <lightrescuer@gmail.com>
2021-01-22 20:09:05 -0600

    Working on landing page v2

--- ml_gatsby/src/lib/landing/LandingPageV2.js
+++ ml_gatsby/src/lib/landing/LandingPageV2.js
@@ -1,6 +1,28 @@
 import React from "react"
-import { Box } from "@chakra-ui/react"
+import { Box, Stack } from "@chakra-ui/react"
+import { MetaTags } from "../components/MetaTags"
+import { Centered, Container } from "../components/commons"
 
-export default function LandingPageV2() {
-  return <Box>I'm new here man</Box>
-}
+export const LandingPageV2 = () => (
+  <Box>
+    <MetaTags />
+    <Container>
+      <IntroSection />
+    </Container>
+  </Box>
+)
+
+const IntroSection = () => (
+  <Stack
+    direction={{ base: "column", md: "row-reverse" }}
+    justify="center"
+    align="center"
+  >
+    <Box w="md" h="md" backgroundColor="brand.500">
+      Hi
+    </Box>
+    <Box w="md" h="md" backgroundColor="secondary.500">
+      Hello
+    </Box>
+  </Stack>
+)
