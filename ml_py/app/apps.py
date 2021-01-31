4690f3472317871211a6ec8fcc633b959251fe92
Akhilez <lightrescuer@gmail.com>
2021-01-30 20:10:39 -0600

    Removed Django stuff

--- ml_py/app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MlgalleryConfig(AppConfig):
-    name = 'app'
--- ml_py/mlg/urls.py
+++
@@ -1,9 +0,0 @@
-# from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    #    path('admin/', admin.site.urls),
-    path('exp/', include('experiments.urls', namespace='exp')),
-    path('api-auth/', include('rest_framework.urls')),
-    path('', include('app.urls', namespace='mlg')),
-]
