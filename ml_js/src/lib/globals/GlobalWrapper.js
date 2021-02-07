90599adf469622ce198a7bc2b08b334df36e745a
Akhilez <lightrescuer@gmail.com>
2021-02-07 17:25:30 -0600

    Removing bug with refocus

--- ml_js/src/lib/globals/GlobalWrapper.js
+++ ml_js/src/lib/globals/GlobalWrapper.js
@@ -1,17 +1,24 @@
 import React from "react"
-import { Box, ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider } from "@chakra-ui/react"
 import theme from "./theme"
 import "src/styles/global.sass"
 import { StaticNavbar } from "../components/navbar"
 import { Footer } from "../components/commons"
 import "typeface-roboto-condensed"
+import { QueryClient, QueryClientProvider } from "react-query"
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false, retry: false } },
+})
 
 const GlobalWrapper = ({ children }) => (
-  <ChakraProvider resetCSS theme={theme}>
-    <StaticNavbar />
-    {children}
-    <Footer />
-  </ChakraProvider>
+  <QueryClientProvider client={queryClient}>
+    <ChakraProvider resetCSS theme={theme}>
+      <StaticNavbar />
+      {children}
+      <Footer />
+    </ChakraProvider>
+  </QueryClientProvider>
 )
 
 export default GlobalWrapper
