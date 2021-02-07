f9df171a7beab1b70e4fbee79461fdbee2c8a48a
Akhilez <lightrescuer@gmail.com>
2021-02-07 17:11:05 -0600

    Working on grid world UI

--- ml_js/src/lib/rl/grid_world/GridWorld.js
+++ ml_js/src/lib/rl/grid_world/GridWorld.js
@@ -1,9 +1,17 @@
-import { Centered } from "../../components/commons"
 import { ProjectWrapper } from "../../components/ProjectWrapper"
 import React from "react"
-import { Link, Text } from "@chakra-ui/react"
+import {
+  Link,
+  Text,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+} from "@chakra-ui/react"
 import { projects } from "src/lib/globals/data"
 import { GridWorldCanvas } from "./GridWorldCanvas"
+import { Container } from "../../components/commons"
 
 export class GridWorld extends React.Component {
   constructor(props) {
@@ -14,10 +22,36 @@ export class GridWorld extends React.Component {
   render() {
     return (
       <ProjectWrapper project={this.project}>
-        <Centered>
-          <Text>Yo! Lets play grid world!</Text>
-          <GridWorldCanvas />
-        </Centered>
+        <GridWorldCanvas />
+        <Container>
+          <Tabs colorScheme="brand">
+            <TabList>
+              <Tab>Policy Gradients</Tab>
+              <Tab>Deep Q</Tab>
+              <Tab>MCTS</Tab>
+              <Tab>AlphaZero</Tab>
+              <Tab>MuZero</Tab>
+            </TabList>
+
+            <TabPanels>
+              <TabPanel>
+                <p>one!</p>
+              </TabPanel>
+              <TabPanel>
+                <p>two!</p>
+              </TabPanel>
+              <TabPanel>
+                <p>three!</p>
+              </TabPanel>
+              <TabPanel>
+                <p>three!</p>
+              </TabPanel>
+              <TabPanel>
+                <p>three!</p>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Container>
       </ProjectWrapper>
     )
   }
--- ml_js/src/lib/rl/grid_world/GridWorldCanvas.js
+++ ml_js/src/lib/rl/grid_world/GridWorldCanvas.js
@@ -1,4 +1,47 @@
 import React from "react"
-import { Box } from "@chakra-ui/react"
+import { Box, Flex, Skeleton, Progress, Text } from "@chakra-ui/react"
+import { Centered } from "../../components/commons"
+import { mlgApi, useGridWorldInitQuery } from "../../api"
+import { SadStates } from "../../components/SadStates"
 
-export const GridWorldCanvas = () => <Box />
+export const GridWorldCanvas = () => {
+  const size = 10
+
+  const {
+    data: initData,
+    isLoading: isInitLoading,
+    isError: isInitError,
+  } = useGridWorldInitQuery()
+
+  const Loader = props => (
+    <Progress
+      value={80}
+      size="sm"
+      w="100px"
+      isIndeterminate
+      colorScheme="brand"
+      {...props}
+    />
+  )
+
+  return (
+    <Box>
+      <Centered>
+        <SadStates
+          when={[
+            {
+              when: isInitLoading,
+              render: <Loader isLoading={isInitLoading} />,
+            },
+            {
+              when: isInitError,
+              render: <Text>Error! Please try again later :(</Text>,
+            },
+          ]}
+        >
+          <Box w="100px" h="100px" backgroundColor="brand.500" />
+        </SadStates>
+      </Centered>
+    </Box>
+  )
+}
