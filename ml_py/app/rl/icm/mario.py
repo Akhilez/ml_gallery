71c279528e0a2824510cc63ff5e26fb18f766e95
Akhilez <lightrescuer@gmail.com>
2021-03-28 19:26:37 -0400

    Working on Mario man!

--- ml_py/app/rl/icm/mario.py
+++ ml_py/app/rl/icm/mario.py
@@ -1,3 +1,6 @@
+from random import shuffle
+from typing import Optional
+
 import gym
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
@@ -11,6 +14,76 @@ import torch
 from torch import nn, optim
 from torch.nn import functional as F
 from collections import deque
+
+
+class MarioModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1),
+            nn.ELU(),
+            nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1),
+            nn.ELU(),
+            nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1),
+            nn.ELU(),
+            nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1),
+            nn.Flatten(),
+            nn.Linear(288, 100),
+            nn.ELU(),
+            nn.Linear(100, 12),
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class ExperienceReplay:
+    def __init__(self, buffer_size: int = 500, batch_size: int = 100):
+        self.buffer_size = buffer_size
+        self.batch_size = batch_size
+
+        self.memory = []
+        self.counter = 0
+
+    def add(
+        self, state1: torch.Tensor, action: int, reward: float, state2: torch.Tensor
+    ):
+        self.counter += 1
+        if self.counter % 500 == 0:
+            shuffle(self.memory)
+
+        memory_tuple = (
+            state1,
+            action,
+            reward,
+            state2,
+        )
+        if len(self.memory) < self.buffer_size:
+            self.memory.append(memory_tuple)
+        else:
+            rand_index = np.random.randint(0, self.buffer_size - 1)
+            self.memory[rand_index] = memory_tuple
+
+    def get_batch(self):
+        batch_size = (
+            len(self.memory) if len(self.memory) < self.batch_size else self.batch_size
+        )
+
+        if len(self.memory) < 1:
+            print("Error: No data in memory.")
+            return None
+
+        indices = np.random.choice(
+            np.arange(len(self.memory)), batch_size, replace=False
+        )
+        batch = [self.memory[i] for i in indices]  # batch is a list of tuples
+
+        state1_batch = torch.stack([x[0].squeeze(dim=0) for x in batch], dim=0)
+        action_batch = torch.LongTensor([x[1] for x in batch])
+        reward_batch = torch.tensor([x[2] for x in batch])
+        state2_batch = torch.stack([x[3].squeeze(dim=0) for x in batch], dim=0)
+
+        return state1_batch, action_batch, reward_batch, state2_batch
 
 
 env = gym_super_mario_bros.make("SuperMarioBros-v0")
@@ -49,3 +122,15 @@ def prepare_initial_state(frame: np.ndarray):
     """
     state = prepare_state(frame).repeat((1, 3, 1, 1))
     return state
+
+
+def sample_action(q_values, epsilon: Optional[float] = None):
+    if epsilon is not None:
+        if torch.rand(1) < epsilon:
+            return torch.randint(low=0, high=len(q_values), size=(1,))
+        else:
+            return torch.argmax(q_values)
+    else:
+        q_values = F.softmax(F.normalize(q_values))
+        sampled_action = torch.multinomial(q_values, num_samples=1)
+        return sampled_action
