bafd8b99bd8afdeda0c8c1719e35f73fe509c005
Akhilez <lightrescuer@gmail.com>
2021-04-02 18:04:26 -0400

    run works fine too

--- ml_py/app/rl/icm/wandb_test.py
+++ ml_py/app/rl/icm/wandb_test.py
@@ -2,21 +2,9 @@ import wandb
 from settings import BASE_DIR
 
 
-def my_train_loop():
-    for epoch in range(10):
-        loss = 0  # change as appropriate :)
-        wandb.log(
-            {
-                "epoch": epoch,
-                "loss": loss,
-                "rewards": wandb.Histogram([1, 4, 3, 5, 6, 3, 2]),
-            }
-        )
-
-
 def main():
 
-    wandb.init(
+    run = wandb.init(
         # name="",  # Name of the run
         project="sample-project",
         save_code=True,
@@ -25,13 +13,14 @@ def main():
         notes=None,  # longer description of run
         dir=BASE_DIR,
     )
-    cfg = wandb.config
+    cfg = run.config
     cfg.post_init = True
     cfg.learning_rate = 0.001
 
-    my_train_loop()
+    for epoch in range(10):
+        loss = 0  # change as appropriate :)
+        run.log({"epoch": epoch, "loss": loss})
 
 
 if __name__ == "__main__":
-
     main()
