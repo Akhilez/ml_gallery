fe4f4a6fbec703f6935e9791e38da8aa1b623e28
Akhilez <lightrescuer@gmail.com>
2021-04-14 19:22:33 -0500

    Started sokoban

--- ml_py/app/rl/dqn/train_sokoban.py
+++ ml_py/app/rl/dqn/train_sokoban.py
@@ -1,29 +1,14 @@
 import gym
-import griddly
 from griddly import gd
-import matplotlib.pyplot as plt
 
-if __name__ != "__main__":
-
-    env = gym.make("GDY-Sokoban-v0")
-    state = env.reset()
-    print(state)
-    print(env.action_space)
-    print(env.observation_space)
-    env.render()
-
-    while True:
-        state, reward, done, info = env.step(env.action_space.sample())
-        env.render()
-        if done:
-            env.reset()
-            env.render()
+from app.rl.dqn.env_wrapper import GriddlyEnvWrapper, TensorStateMixin
 
 
-if __name__ == "__main__":
-    env = gym.make(f"GDY-Sokoban-v0", global_observer_type=gd.ObserverType.SPRITE_2D)
-    state = env.reset()
-    print(state)
-    print(env.action_space)
-    print(env.observation_space)
-    # env.render()
+class SokobanEnvWrapper(GriddlyEnvWrapper, TensorStateMixin):
+    def __init__(self):
+        super().__init__()
+        self.env = gym.make(
+            "GDY-Sokoban-v0",
+            global_observer_type=gd.ObserverType.VECTOR,
+            player_observer_type=gd.ObserverType.VECTOR,
+        )
